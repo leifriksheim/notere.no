078d094e439ee6e829eed69c948a6650f2f46750
leifriksheim <riksheim.leif@gmail.com>
2020-06-17 15:18:00 +0200

    updates

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 import options from "./options.js";
-import { Toggle } from "https://unpkg.com/core-elements@0.0.24/dist/components/toggle.js";
+
+import { Toggle } from "https://unpkg.com/core-elements@latest/dist/components/toggle.js";
+import {
+  Tabs,
+  Tab,
+} from "https://unpkg.com/core-elements@latest/dist/components/tabs.js";
+import { Button } from "https://unpkg.com/core-elements@latest/dist/components/button.js";
 
 customElements.define("core-toggle", Toggle);
+customElements.define("core-tabs", Tabs);
+customElements.define("core-tab", Tab);
+customElements.define("core-button", Button);
 
-const initTheme = localStorage.getItem("theme");
-setTheme(initTheme);
+const initMode = localStorage.getItem("mode");
+setMode(initMode);
 
 const toggleButton = document.querySelector(".toggle");
-toggleButton.checked = initTheme === "dark";
+toggleButton.checked = initMode === "dark";
 
-function setTheme(val) {
-  document.body.setAttribute("theme", val);
-  localStorage.setItem("theme", val);
+function setMode(val) {
+  document.body.setAttribute("mode", val);
+  localStorage.setItem("mode", val);
 }
 
 toggleButton.addEventListener("change", (e) => {
-  setTheme(e.target.checked ? "dark" : "light");
+  setMode(e.target.checked ? "dark" : "light");
 });
 
 var editor = new MediumEditor(".editable", options);
